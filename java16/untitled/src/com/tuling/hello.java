53f8e92113c828be2d8dc56e1b617969cd759302
fred <fred@qq.com>
2019-10-25 11:55:10 +0800

    修改了文件

--- java16/untitled/src/com/tuling/hello.java
+++ java16/untitled/src/com/tuling/hello.java
@@ -3,6 +3,15 @@ package com.tuling;
 public class hello {
 
     public static void main(String[] args) {
+
         System.out.println("00000000000000");
+
+
+        System.out.println("hahahaha");
+    }
+    public void show() {
+        for (int i = 0; i < 10; i++){
+            System.out.println("******");
+        }
     }
 }
